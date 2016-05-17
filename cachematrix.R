ec2736ebe94ac9994b42b13ead7d2f04258aef91
tennistetris <shubhamdhal@gmail.com>
2016-05-17 15:46:44 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv = NULL
+  get <- function()x
+  set <- function(y){
+    x<<-y
+    inv <<-  NULL
+  }
+  getinv <- function()inv
+  setinv <- function(solve) inv<<- solve
+  list(get = get , set = set , getinv = getinv , setinv = setinv)
 }
 
 
@@ -12,4 +20,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ##The previous comment is a bit confusing , insn't x a list and not a matrix ??
+  inv <- x$getinv()
+  if(!is.null(inv)){
+    message("returning cached inverse")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat)
+  x$setinv(inv)
+  inv
 }
